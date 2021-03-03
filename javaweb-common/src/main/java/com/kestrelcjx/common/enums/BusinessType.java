ea068fb96720e16da7a11512435cd299df299a89
Kestrel <2366540348@qq.com>
2021-03-03 11:52:11 +0800

    add BusinessType enum

---
+++ javaweb-common/src/main/java/com/kestrelcjx/common/enums/BusinessType.java
@@ -0,0 +1,66 @@
+package com.kestrelcjx.common.enums;
+
+/**
+ * 日志类型
+ */
+public enum BusinessType {
+    /**
+     * 其他
+     */
+    OTHER,
+
+    /**
+     * 新增
+     */
+    INSERT,
+
+    /**
+     * 修改
+     */
+    UPDATE,
+
+    /**
+     * 删除
+     */
+    DELETE,
+
+    /**
+     * 授权
+     */
+    GRANT,
+
+    /**
+     * 导出
+     */
+    EXPORT,
+
+    /**
+     * 导入
+     */
+    IMPORT,
+
+    /**
+     * 强制
+     */
+    FORCE,
+
+    /**
+     * 生成代码
+     */
+    GENCODE,
+
+    /**
+     * 清空数据
+     */
+    CLEAN,
+
+    /**
+     * 状态
+     */
+    STATUS,
+
+    /**
+     * 批量删除
+     */
+    BATCH_DELETE,
+}
