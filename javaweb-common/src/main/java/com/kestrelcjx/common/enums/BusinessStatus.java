a41ed90432dea1078e0452ae18ed55330fe620c8
Kestrel <2366540348@qq.com>
2021-03-03 13:36:00 +0800

    add BusinessStatus enum

---
+++ javaweb-common/src/main/java/com/kestrelcjx/common/enums/BusinessStatus.java
@@ -0,0 +1,18 @@
+package com.kestrelcjx.common.enums;
+
+public enum BusinessStatus {
+    /**
+     * 默认值
+     */
+    DEFAULT,
+
+    /**
+     * 成功
+     */
+    SUCCESS,
+
+    /**
+     * 失败
+     */
+    FAIL,
+}
