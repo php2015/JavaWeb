051c34a777fbb282ab7277993acab0be2d2a4e81
Kestrel <2366540348@qq.com>
2021-03-03 12:21:06 +0800

    add annotations

---
+++ javaweb-common/src/main/java/com/kestrelcjx/common/annotation/Log.java
@@ -0,0 +1,34 @@
+package com.kestrelcjx.common.annotation;
+
+import com.kestrelcjx.common.enums.BusinessType;
+import com.kestrelcjx.common.enums.OperatorType;
+
+import java.lang.annotation.*;
+
+/**
+ * 自定义操作日志注解
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.PARAMETER, ElementType.METHOD})
+@Documented
+public @interface Log {
+    /**
+     * 模块
+     */
+    String title() default "";
+
+    /**
+     * 功能
+     */
+    BusinessType businessType() default BusinessType.OTHER;
+
+    /**
+     * 操作人员类别
+     */
+    OperatorType operatorType() default OperatorType.MANAGE;
+
+    /**
+     * 是否保存请求的参数
+     */
+    boolean isSaveRequestData() default true;
+}
