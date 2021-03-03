cab1b2f2738436c61d5d258d56012d6ae469946d
Kestrel <2366540348@qq.com>
2021-03-03 12:11:54 +0800

    add OperatorType enum

---
+++ javaweb-common/src/main/java/com/kestrelcjx/common/enums/OperatorType.java
@@ -0,0 +1,21 @@
+package com.kestrelcjx.common.enums;
+
+/**
+ * 操作人员类别
+ */
+public enum OperatorType {
+    /**
+     * 其他
+     */
+    OTHER,
+
+    /**
+     * 后台用户
+     */
+    MANAGE,
+
+    /**
+     * 手机端用户
+     */
+    MOBILE,
+}
