ca92c58cdee912906786d0864b8c5287a9eb9137
Kestrel <2366540348@qq.com>
2021-03-07 12:11:38 +0800

    add RedisConfig

---
+++ javaweb-common/src/main/java/com/kestrelcjx/common/config/RedisConfig.java
@@ -0,0 +1,69 @@
+package com.kestrelcjx.common.config;
+
+import com.kestrelcjx.common.utils.RedisUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+@PropertySource("classpath:redis.properties")
+public class RedisConfig {
+    @Value("${redis.hostName}")
+    private String hostName;
+    @Value("${redis.port}")
+    private Integer port;
+    @Value("${redis.password}")
+    private String password;
+    @Value("${redis.timeout}")
+    private Integer timeout;
+
+    /**
+     * lettuce实现redis方式
+     *
+     * @param redisConnectFactory
+     * @return
+     */
+    @Bean
+    public RedisTemplate<String, Object> redisCacheTemplate(LettuceConnectionFactory redisConnectFactory) {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        initDomainRedisTemplate(redisTemplate, redisConnectFactory);
+        return redisTemplate;
+    }
+
+    /**
+     * 设置数据注入redis的序列化方式，并开启事务
+     *
+     * @param redisTemplate
+     * @param factory
+     */
+    private void initDomainRedisTemplate(RedisTemplate<String, Object> redisTemplate,
+                                         RedisConnectionFactory factory) {
+        // 如果不配置Serializer，那么存储时缺省使用String
+        // 如果用User，那么会提示错误：User can't cast to String!
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
+        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        // 开启事务
+        redisTemplate.setEnableTransactionSupport(true);
+        redisTemplate.setConnectionFactory(factory);
+    }
+
+    /**
+     * 注入数据RedisTemplate
+     *
+     * @param redisTemplate
+     * @return
+     */
+    public RedisUtils redisUtils(RedisTemplate<String, Object> redisTemplate) {
+        RedisUtils redisUtils = new RedisUtils();
+        redisUtils.setRedisTemplate(redisTemplate);
+        return redisUtils;
+    }
+}
