b0a2d7f11eedd7d5046bb6f91e3b20926cecd1b7
Kestrel <2366540348@qq.com>
2021-03-07 11:07:21 +0800

    add ExcelUtilTest

---
+++ javaweb-common/src/test/java/com/kestrelcjx/common/utils/ExcelUtilTest.java
@@ -0,0 +1,44 @@
+package com.kestrelcjx.common.utils;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ExcelUtilTest {
+    @Test
+    public void testReadLessThan1000Row() {
+        List<Object> list = ExcelUtil.readLessThan1000Row(
+                "C:\\Users\\Administrator\\Desktop\\" +
+                        "JavaWeb\\javaweb-common\\src\\test\\resources\\excel.xlsx");
+        for (Object o : list) {
+            System.out.println(o);
+        }
+    }
+
+    @Test
+    public void testReadMoreThan1000Row() {
+        List<Object> list = ExcelUtil.readLessThan1000Row(
+                "C:\\Users\\Administrator\\Desktop\\" +
+                        "JavaWeb\\javaweb-common\\src\\test\\resources\\excels.xlsx");
+        for (Object o : list) {
+            System.out.println(o);
+        }
+    }
+
+    @Test
+    public void testWriteBySimple() {
+        List<List<Object>> data = new ArrayList<>();
+        List<String> head = new ArrayList<>();
+        head.add("壹");
+        head.add("贰");
+        head.add("叁");
+        List<Object> item = new ArrayList<>();
+        item.add("1");
+        item.add("2");
+        item.add("3");
+        data.add(item);
+        ExcelUtil.writeBySimple("C:\\Users\\Administrator\\Desktop\\" +
+                "JavaWeb\\javaweb-common\\src\\test\\resources\\export.xlsx", data, head);
+    }
+}
