8ecf0a7ce5ccef131c90b102016c4f12898bd7e3
Kestrel <2366540348@qq.com>
2021-03-13 22:33:39 +0800

    modified AdminApplication

--- javaweb-admin/src/main/java/com/kestrelcjx/admin/AdminApplication.java
+++ javaweb-admin/src/main/java/com/kestrelcjx/admin/AdminApplication.java
@@ -1,8 +1,10 @@
 package com.kestrelcjx.admin;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan("com.kestrelcjx.**.mapper")
 @SpringBootApplication(scanBasePackages = {"com.kestrelcjx.*"})
 public class AdminApplication {
     public static void main(String[] args) {
