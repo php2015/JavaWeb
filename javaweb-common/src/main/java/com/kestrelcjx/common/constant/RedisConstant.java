58e55a70ee56c83b3d2a1909412aff9b1d2974e9
Kestrel <2366540348@qq.com>
2021-03-14 13:10:39 +0800

    modified RedisConstant

--- javaweb-common/src/main/java/com/kestrelcjx/common/constant/RedisConstant.java
+++ javaweb-common/src/main/java/com/kestrelcjx/common/constant/RedisConstant.java
@@ -4,21 +4,21 @@ public class RedisConstant {
     /**
      * TOKEN前缀
      */
-    String REDIS_PREFIX_LOGIN = "login_token_%s";
+    public static String REDIS_PREFIX_LOGIN = "login_token_%s";
     /**
      * 过期时间2小时
      */
-    Integer REDIS_EXPIRE_TWO = 7200;
+    public static Integer REDIS_EXPIRE_TWO = 7200;
     /**
      * 过期时间15分钟
      */
-    Integer REDIS_EXPIRE_EMAIL = 900;
+    public static Integer REDIS_EXPIRE_EMAIL = 900;
     /**
      * 过期时间5分钟
      */
-    Integer REDIS_EXPIRE_CAPTCHA = 300;
+    public static Integer REDIS_EXPIRE_CAPTCHA = 300;
     /**
      * 暂无过期时间
      */
-    Integer REDIS_EXPIRE_NULL = -1;
+    public static Integer REDIS_EXPIRE_NULL = -1;
 }
