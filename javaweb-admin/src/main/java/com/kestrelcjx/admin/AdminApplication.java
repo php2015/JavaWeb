7f0401e661f442f7c0f7103727a82a4a9080dfbb
Kestrel <2366540348@qq.com>
2021-03-15 12:37:25 +0800

    add @EnableTransactionManagement

--- javaweb-admin/src/main/java/com/kestrelcjx/admin/AdminApplication.java
+++ javaweb-admin/src/main/java/com/kestrelcjx/admin/AdminApplication.java
@@ -3,9 +3,11 @@ package com.kestrelcjx.admin;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @MapperScan("com.kestrelcjx.**.mapper")
 @SpringBootApplication(scanBasePackages = {"com.kestrelcjx.*"})
+@EnableTransactionManagement
 public class AdminApplication {
     public static void main(String[] args) {
         SpringApplication.run(AdminApplication.class, args);
